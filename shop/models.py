480f7f44c90a053011a9357e6fda7ab0f0163ac1
Jimi <69767677+JaaMata@users.noreply.github.com>
2021-04-16 14:17:28 +0100

    magic

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    description = models.CharField(max_length=500, null=True)
+    img = models.ImageField(upload_to="item_image", null=True)
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def items(request):
+    items = Item.objects.all()
+
+    context = {'items': items}
+    return render(request, "items.html", context)
+
